c28eadc2950099e7ae00627b5c7f2e040f2a39f2
CutieDeng <CutieDeng@iCloud.com>
2023-09-12 20:58:44 +0800

    Finish zig build script, to generate binary.
    - Use `zig objcopy` to generated binary format elf files.

--- build.zig
+++ build.zig
@@ -3,7 +3,6 @@ const builtin = @import("builtin");
 const std = @import("std");
 
 pub fn build(b: *Builder) void {
-
     // Target STM32F407VG
     const target : std.zig.CrossTarget = .{
         .cpu_arch = .thumb,
@@ -11,34 +10,36 @@ pub fn build(b: *Builder) void {
         .os_tag = .freestanding,
         .abi = .eabihf,
     };
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode : std.builtin.Mode = b.standardOptimizeOption(.{}); 
-
     const elf : *std.Build.Step.Compile = b.addExecutable(.{
         .name = "zig-stm32-blink.elf", 
         .root_source_file = .{ .path = "src/startup.zig" },
         .target = target, 
         .optimize = mode, 
     });
-
     const vector_obj : *std.Build.Step.Compile = b.addObject( .{
         .name = "vector", 
         .root_source_file = .{ .path = "src/vector.zig" }, 
         .target = target, 
         .optimize = mode, 
     }) ;
-
     elf.addObject(vector_obj);
     elf.setLinkerScriptPath(.{ .path = "src/linker.ld" });
-
-    // const bin = b.addInstallRaw(elf, "zig-stm32-blink.bin", .{});
-    // const ibn = b.addInstallBinFile(source: LazyPath, dest_rel_path: []const u8)
+    const artifact: *std.Build.Step.InstallArtifact = b.addInstallArtifact(elf, .{});
+    b.default_step.dependOn(&elf.step);
+    b.default_step.dependOn(&artifact.step); 
+    const cmd : *std.Build.Step.Run = b.addSystemCommand(&[_][]const u8{
+        "zig", 
+        "objcopy",
+        "-O", 
+        "binary", 
+        "zig-out/bin/zig-stm32-blink.elf", 
+        "zig-out/bin/zig-stm32-blink.bin",
+    }); 
     const bin_step : *std.build.Builder.Step = b.step("bin", "Generate binary file to be flashed");
-    _ = bin_step;
-    // bin_step.dependOn(&bin.step);
-
+    bin_step.dependOn(&cmd.step);
+    cmd.step.dependOn(b.default_step); 
+    b.default_step.dependOn(&elf.step);
     // const flash_cmd = b.addSystemCommand(&[_][]const u8{
     //     "st-flash",
     //     "write",
@@ -48,7 +49,4 @@ pub fn build(b: *Builder) void {
     // flash_cmd.step.dependOn(&bin.step);
     // const flash_step = b.step("flash", "Flash and run the app on your STM32F4Discovery");
     // flash_step.dependOn(&flash_cmd.step);
-
-    b.default_step.dependOn(&elf.step);
-    b.installArtifact(elf);
 }
